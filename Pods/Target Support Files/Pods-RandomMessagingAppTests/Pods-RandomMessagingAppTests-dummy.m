d2254bac162ee520bd7567612053eb871994a990
Yusuf <yusuf@eftsoftware.com>
2017-07-26 10:21:57 +0300

    Pods are initialized

---
+++ Pods/Target Support Files/Pods-RandomMessagingAppTests/Pods-RandomMessagingAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RandomMessagingAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_RandomMessagingAppTests
+@end
---
+++ RandomMessagingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomMessagingApp
+//
+//  Created by Yusuf Miletli on 7/26/17.
+//  Copyright © 2017 Miletli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
