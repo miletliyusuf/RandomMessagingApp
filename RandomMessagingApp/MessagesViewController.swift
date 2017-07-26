eb676d8d80dee94d8dd3828c0464dfd7ebeda85b
Yusuf <yusuf@eftsoftware.com>
2017-07-26 10:31:49 +0300

    View controllers and their views are added

--- RandomMessagingApp/MessagesViewController.swift
+++ RandomMessagingApp/MessagesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MessagesViewController.swift
 //  RandomMessagingApp
 //
 //  Created by Yusuf Miletli on 7/26/17.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MessagesViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
 }
-
